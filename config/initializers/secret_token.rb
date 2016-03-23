f74909215b60640ccfb05f2674037b1a7fa582a0
Melissa Liu <mliu@2025labs.com>
2016-03-23 12:59:30 -0700

    first cut of layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consulting::Application.config.secret_key_base = '650b0b507e258cabb69a95ee1000091bd86c2245be5a7aca08cf191e05a2f7e00b941579387f334d7f65715c91374f2fe80b91901d7d7cfe1a3596b2ebbb2708'
