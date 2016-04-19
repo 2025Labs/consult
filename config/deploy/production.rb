be26a2979a1644cf213a88c9be03484ee0d7a087
Melissa Liu <mliu@2025labs.com>
2016-04-19 10:28:13 -0700

    ips

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :domain, "www.2025consulting.com"  # 
 
-role :app, "52.35.43.116"
-role :web, "52.35.43.116"
-role :worker, "52.35.43.116"
+role :app, "52.27.238.71"
+role :web, "52.27.238.71"
+role :worker, "52.27.238.71"
 
-server "52.35.43.116", :db, :primary=>true
+server "52.27.238.71", :db, :primary=>true
 
